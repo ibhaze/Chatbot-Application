ca5f4cf03337db3673d7a656b0d867ed515efd01
”IbhazeGit config --global user.email I510304@fhict.nlGit config --global user.name ”Ibhaze <i510304@student.fontys.nl>
2024-09-24 10:06:36 +0200

    ll

--- chatbot/lib/home_page.dart
+++ chatbot/lib/home_page.dart
@@ -1,6 +1,11 @@
+ 
+
+import 'package:animate_do/animate_do.dart';
 import 'package:chatbot/feature_box.dart';
+import 'package:chatbot/openai_service.dart';
 import 'package:flutter/material.dart';
 import 'package:chatbot/pallete.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 
@@ -13,11 +18,24 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final speechToText = SpeechToText();
+  final flutterTts = FlutterTts();
   String lastWords = '';
+  final OpenAIService openAIService = OpenAIService();
+  String? generatedContent;
+  String? generatedImageUrl;
+  int start = 200;
+  int delay = 200;
+
   @override
   void initState() {
     super.initState();
     initSpeechToText();
+    initTextToSpeech();
+  }
+
+  Future<void> initTextToSpeech() async {
+    await flutterTts.setSharedInstance(true);
+    setState(() {});
   }
 
   Future<void> initSpeechToText() async {
@@ -48,18 +66,24 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  Future<void> systemSpeak(String content) async {
+    await flutterTts.speak(content);
+  }
+
   @override
   void dispose() {
- 
     super.dispose();
     speechToText.stop(); //if the screen has been stopped
+    flutterTts.stop();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('pie'), // This is the title on the top of the app
+          title: BounceInDown(
+            child: const Text('pie'),
+          ), // This is the title on the top of the app
           leading: const Icon(Icons.menu), // Creates a menu
           iconTheme: const IconThemeData(color: Colors.white), // Icon color
           titleTextStyle: const TextStyle(
@@ -72,88 +96,123 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             children: [
               //Bot picture
-              Center(
-                child: Container(
-                  height: 120,
-                  width: 120,
-                  margin: const EdgeInsets.only(top: 20), // Keeps the margin
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle, // Makes the container circular
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/Alien01.png'),
+              ZoomIn(
+                child: Center(
+                  child: Container(
+                    height: 120,
+                    width: 120,
+                    margin: const EdgeInsets.only(top: 20), // Keeps the margin
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle, // Makes the container circular
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/Alien01.png'),
+                      ),
                     ),
                   ),
                 ),
               ),
               //Chat bubble
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal:
-                      30, // Adds padding to the left and right of the text inside the text container
-                  vertical:
-                      20, // Adds padding to the top and bottom of the text inside the text container
-                ),
-                margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
-                  top: 30,
-                ), // Keeps the margin
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Pallete.borderColor,
-                  ),
-                  borderRadius: BorderRadius.circular(20).copyWith(
-                    topLeft: Radius
-                        .zero, //This makes every edge of the box circular apart from the top left
-                  ),
-                ),
-                child: const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10.0),
-                  child: Text(
-                    'Good morning , what task can I do for you?',
-                    style: TextStyle(
-                      fontFamily: 'Chivo',
-                      color: Pallete.whiteColor,
-                      fontSize: 20,
+              FadeInRight(
+                child: Visibility(
+                  visible: generatedImageUrl == null,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal:
+                          30, // Adds padding to the left and right of the text inside the text container
+                      vertical:
+                          20, // Adds padding to the top and bottom of the text inside the text container
+                    ),
+                    margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
+                      top: 30,
+                    ), // Keeps the margin
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Pallete.borderColor,
+                      ),
+                      borderRadius: BorderRadius.circular(20).copyWith(
+                        topLeft: Radius
+                            .zero, //This makes every edge of the box circular apart from the top left
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10.0),
+                      child: Text(
+                        generatedContent == null
+                            ? 'Good morning , what task can I do for you?'
+                            : generatedContent!,
+                        style: TextStyle(
+                          fontFamily: 'Chivo',
+                          color: Pallete.whiteColor,
+                          fontSize: generatedContent == null ? 20 : 18,
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ),
-              Container(
-                padding: const EdgeInsets.all(
-                    18), //since alignment center left moves it too far to the left , this will create padding around the text
-                alignment: Alignment.centerLeft,
-                child: const Text(
-                  'Here are a few features',
-                  style: TextStyle(
-                    fontFamily: 'Chivo',
-                    color: Pallete.borderColor,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+              if (generatedImageUrl != null)
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(generatedImageUrl!),
+                  ),
+                ),
+              SlideInLeft(
+                child: Visibility(
+                  visible:
+                      generatedContent == null && generatedImageUrl == null,
+                  child: Container(
+                    padding: const EdgeInsets.all(
+                        18), //since alignment center left moves it too far to the left , this will create padding around the text
+                    alignment: Alignment.centerLeft,
+                    child: const Text(
+                      'Here are a few features',
+                      style: TextStyle(
+                        fontFamily: 'Chivo',
+                        color: Pallete.borderColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
                 ),
               ),
 
               //Bot suggestions
-              Column(
-                children: const [
-                  FeatureBox(
-                    color: Pallete.mainFontColor,
-                    headerText: 'ChatGPT',
-                    descriptionText:
-                        'A faster way to go around find answers to every question with ChatGPT',
-                  ),
-                  FeatureBox(
-                    color: Pallete.firstSuggestionBoxColor,
-                    headerText: 'Dall-E',
-                    descriptionText:
-                        'Get Inspired & stay creative with your personal assistant powered by Dall-E',
-                  ),
-                  FeatureBox(
-                    color: Pallete.thirdSuggestionBoxColor,
-                    headerText: 'Smart Voice Assistant',
-                    descriptionText:
-                        'Get the best of both worlds with a voice assistant powered by Dall-E & ChatGPT ',
-                  ),
-                ],
+              Visibility(
+                visible: generatedContent == null && generatedImageUrl == null,
+                child: Column(
+                  children: [
+                    SlideInLeft(
+                      delay: Duration(milliseconds: start),
+                      child: const FeatureBox(
+                        color: Pallete.mainFontColor,
+                        headerText: 'ChatGPT',
+                        descriptionText:
+                            'A faster way to go around find answers to every question with ChatGPT',
+                      ),
+                    ),
+                    SlideInLeft(
+                      delay: Duration(milliseconds: start+delay),//this delays the entrance of the second box 
+                      child:  const FeatureBox(
+                        color: Pallete.firstSuggestionBoxColor,
+                        headerText: 'Dall-E',
+                        descriptionText:
+                            'Get Inspired & stay creative with your personal assistant powered by Dall-E',
+                      ),
+                    ),
+                    SlideInLeft(
+                      delay: Duration(milliseconds: start+ 2* delay),
+                      child: const FeatureBox(
+                        color: Pallete.thirdSuggestionBoxColor,
+                        headerText: 'Smart Voice Assistant',
+                        descriptionText:
+                            'Get the best of both worlds with a voice assistant powered by Dall-E & ChatGPT ',
+                      ),
+                    ),
+                  ],
+                ),
               )
             ],
           ),
@@ -193,17 +252,30 @@ class _HomePageState extends State<HomePage> {
                     speechToText.isNotListening) {
                   await startListening();
                 } else if (speechToText.isListening) {
+                  final speech = await openAIService.isArtPromptAPI(lastWords);
+                  if (speech.contains('https')) {
+                    generatedImageUrl = speech;
+                    generatedContent = null;
+                  } else {
+                    generatedImageUrl = null;
+                    generatedContent = speech;
+                    setState(() {});
+                    await systemSpeak(speech);
+                  }
+
                   await stopListening();
                 } else {
                   initSpeechToText();
-                }//this just means that if the button is clicked and the app has permission and the mic isnt already listening , it should start listening , and if its already listening it should stop else the function should be run 
+                } //this just means that if the button is clicked and the app has permission and the mic isnt already listening , it should start listening , and if its already listening it should stop else the function should be run
               },
               backgroundColor:
                   Colors.black, // Set the button's background to black
               elevation:
                   0, // Removes the default FloatingActionButton shadow to avoid conflicts with custom shadow
-              child: const Icon(
-                Icons.mic, // Mic icon inside the button
+              child: Icon(
+                speechToText.isListening
+                    ? Icons.stop
+                    : Icons.mic, // Mic icon inside the button
                 color: Colors.purple, // Set the mic icon color to purple
               ),
             ),
--- chatbot/lib/secrets.dart
+++ chatbot/lib/secrets.dart
@@ -1,2 +1,2 @@
-const openAIAPIKey = 'sk-LDU77mcgSPSA7r_WB-UXVjDF9qwux7n-sLg4FzCSu1T3BlbkFJ43PxA4u4Le4ZH5IqY9E_o9D2As7ccr-ufihPcjb4IA';
+const openAIAPIKey = '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';
  
